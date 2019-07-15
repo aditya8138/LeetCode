b620f527a6d49ce1b71232cae1e0259e629af40a
Aditya <aditya8138@gmail.com>
2019-07-15 10:11:10 -0500

    removing main.java

---
+++ src/medium/SearchInRotatedSortedArray_33.java
@@ -0,0 +1,32 @@
+package medium;
+
+public class SearchInRotatedSortedArray {
+    public static int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[lo] <= nums[mid]) {
+                if (target < nums[mid] && target >= nums[lo]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+            if (nums[hi] >= nums[mid]) {
+                if (target <= nums[hi] && target > nums[mid]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
