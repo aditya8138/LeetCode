ee6654048efd09b2f6fd7cae33de5deb5da3bcf8
Aditya <aditya8138@gmail.com>
2019-07-24 16:19:57 -0500

    169

---
+++ src/easy/MajorityElement_169.java
@@ -0,0 +1,44 @@
+package easy;
+
+
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+        You may assume that the array is non-empty and the majority element always exist in the array.
+
+        Example 1:
+
+        Input: [3,2,3]
+        Output: 3
+        Example 2:
+
+        Input: [2,2,1,1,1,2,2]
+        Output: 2
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement_169 {
+    public static int majorityElement(int[] nums) {
+        if (nums == null) {
+            return -1;
+        }
+        int majority = nums.length / 2;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            if (countMap.get(num) == null) {
+                countMap.put(num, 1);
+            } else {
+                countMap.put(num, countMap.get(num) + 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() > majority) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+}
