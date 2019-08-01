30d070d2552bc489726d2cbd965c8db752473d68
Aditya <aditya8138@gmail.com>
2019-08-01 10:24:51 -0500

    590

---
+++ src/easy/N_aryTreePostorderTraversal_590.java
@@ -0,0 +1,36 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class N_aryTreePostorderTraversal_590 {
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> postorder(Node root) {
+
+        if (root == null) {
+            return res;
+        }
+
+        for (Node n : root.children) {
+            postorder(n);
+
+        }
+        res.add(root.val);
+        return res;
+    }
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+}
