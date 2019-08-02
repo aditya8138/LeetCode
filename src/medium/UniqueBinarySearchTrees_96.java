ffadd9fbdedff47868fd789c58a9d065b266f1cf
Aditya <aditya8138@gmail.com>
2019-08-02 11:20:57 -0500

    96

---
+++ src/medium/UniqueBinarySearchTrees_96.java
@@ -0,0 +1,16 @@
+package medium;
+
+public class UniqueBinarySearchTrees_96 {
+    public static int numTrees(int n) {
+        int[] trees = new int[n + 1];
+        trees[0] = 1;
+        trees[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                trees[i] += trees[j - 1] * trees[i - j];
+            }
+        }
+        return trees[n];
+    }
+}
