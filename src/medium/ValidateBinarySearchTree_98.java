35b5c4eca0f0f1bdb8b3d9111a202d0508ce7a70
Aditya <aditya8138@gmail.com>
2019-08-05 21:41:09 -0500

    98

---
+++ src/medium/ValidateBinarySearchTree_98.java
@@ -0,0 +1,37 @@
+package medium;
+
+import java.util.Stack;
+
+public class ValidateBinarySearchTree_98 {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+        double inorder = -Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // If next element in inorder traversal
+            // is smaller than the previous one
+            // that's not BST.
+            if (root.val <= inorder) {
+                return false;
+            }
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
